b61e6ca0b10935adcd75619a330ae5acb03cd43b
Job Dorado <jobdorado15@gmail.com>
2021-10-19 22:42:25 -0500

    heroku

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use(auth);
 app.use("/empleado", empleado);
 app.use(notFound);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 });
